4e308dc826998c8271e9a5b717ebf12a9dcfeb10
levaniakobidze <levaniakobidze25@gmail.com>
2022-05-06 15:53:04 +0400

    adsad

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
   const [colorChange, setColorchange] = useState(false);
 
   const changeNavbarColor = () => {
-    if (window.scrollY >= 80) {
+    if (window.scrollY >= 800) {
       setColorchange(true);
     } else {
       setColorchange(false);
