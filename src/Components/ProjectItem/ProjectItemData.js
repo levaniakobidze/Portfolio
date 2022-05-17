10beeb9bd7f0dee7343c0fd23582457f7d7ff372
levaniakobidze <levaniakobidze25@gmail.com>
2022-05-17 20:25:13 +0400

    dasdasd

--- src/Components/ProjectItem/ProjectItemData.js
+++ src/Components/ProjectItem/ProjectItemData.js
@@ -1,10 +1,24 @@
 export const data = [
   {
-    id: 1,
-    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1649954772/landing_v1fmpo.png",
-    host: "https://travel-page1.netlify.app/",
-    title: "Travel page",
-    github: "https://github.com/levaniakobidze/Travel-page",
+    id: 6,
+    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1651179636/ytclone_uuueoy.png",
+    host: "https://clonetube1.netlify.app/",
+    title: "YouTube clone",
+    github: "https://github.com/levaniakobidze/Youtube-Clone",
+  },
+  {
+    id: 9,
+    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1652800332/country_dcbmzo.png",
+    host: "https://country123.netlify.app//",
+    title: "Search country",
+    github: "https://github.com/levaniakobidze/Country-search-API",
+  },
+  {
+    id: 5,
+    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1650572253/todoo_zt2uiz.png",
+    host: "https://todo-app-with-react12.netlify.app/",
+    title: "Todo-App",
+    github: "https://github.com/levaniakobidze/React-Todo-app",
   },
   {
     id: 2,
@@ -13,6 +27,21 @@ export const data = [
     title: "Users search",
     github: "https://github.com/levaniakobidze/Github-Users",
   },
+  {
+    id: 7,
+    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1651425412/traff11_niyc3h.png",
+    host: "https://ltrafalgar.netlify.app/",
+    title: "Landing page",
+    github: "https://github.com/levaniakobidze/trafalgar-landing-page",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1649954772/landing_v1fmpo.png",
+    host: "https://travel-page1.netlify.app/",
+    title: "Travel page",
+    github: "https://github.com/levaniakobidze/Travel-page",
+  },
+
   {
     id: 3,
     img: "https://res.cloudinary.com/diyky5uya/image/upload/v1650569021/landing-page_fcq4fi.png",
@@ -27,37 +56,10 @@ export const data = [
     title: "Quotes generator",
     github: "https://github.com/levaniakobidze/React-Random-Quotes",
   },
-  {
-    id: 5,
-    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1650572253/todoo_zt2uiz.png",
-    host: "https://todo-app-with-react12.netlify.app/",
-    title: "Todo-App",
-    github: "https://github.com/levaniakobidze/React-Todo-app",
-  },
-  {
-    id: 6,
-    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1651179636/ytclone_uuueoy.png",
-    host: "https://clonetube1.netlify.app/",
-    title: "YouTube clone",
-    github: "https://github.com/levaniakobidze/Youtube-Clone",
-  },
-  {
-    id: 7,
-    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1651425412/traff11_niyc3h.png",
-    host: "https://ltrafalgar.netlify.app/",
-    title: "Landing page",
-    github: "https://github.com/levaniakobidze/trafalgar-landing-page",
-  },
+
   {
     id: 8,
     img: "https://res.cloudinary.com/diyky5uya/image/upload/v1651425412/traff11_niyc3h.png",
-    host: "https://clonetube1.netlify.app/",
-    title: "YouTube clone",
-    github: "https://github.com/levaniakobidze/Youtube-Clone",
-  },
-  {
-    id: 9,
-    img: "https://res.cloudinary.com/diyky5uya/image/upload/v1651179636/ytclone_uuueoy.png",
     host: "https://clonetube1.netlify.app/",
     title: "YouTube clone",
     github: "https://github.com/levaniakobidze/Youtube-Clone",
