162ce233fb2cb27c78e9907ec7dd0effc9f189f8
Levan iakobidze <levaniakobidze25@gmail.com>
2022-09-23 15:21:30 +0400

    add styleing on project (Quiz-api)

--- src/Components/ProjectItem/ProjectItemData.js
+++ src/Components/ProjectItem/ProjectItemData.js
@@ -23,7 +23,7 @@ export const data = [
   {
     id: 8,
     img: "https://res.cloudinary.com/diyky5uya/image/upload/v1653212597/quiiz_ckvfzn.png",
-    host: "https://levanquiz.netlify.app/",
+    host: "https://quiz-api-levaniakobidze.vercel.app/",
     title: "Quiz ( api )",
     github: "https://github.com/levaniakobidze/Quiz_api",
   },
